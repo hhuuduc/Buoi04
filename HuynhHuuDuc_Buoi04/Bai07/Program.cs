cde8eb3e34d10034198b1fe01757952fcaf7994c
A209PC39\Administrator <huynhhuuduc5031@gmail.com>
2023-09-06 13:51:49 +0700

    update bai07

--- HuynhHuuDuc_Buoi04/Bai07/Program.cs
+++ HuynhHuuDuc_Buoi04/Bai07/Program.cs
@@ -10,6 +10,13 @@ namespace Bai07
     {
         static void Main(string[] args)
         {
+            ToaDo td = new ToaDo();
+            td.Ten = "O";
+            td.X = 5;
+            td.Y = 5;
+            HinhTron ht = new HinhTron(td, 10.5);
+            Console.WriteLine("Hình tròn có tâm {0}({1},{2}) với bán kính {3} có diện tích và chu vi lần lượt là {4} và {5}", td.Ten, td.X, td.Y, ht.BanKinh, Math.Round(ht.tinhDienTich(),3), Math.Round(ht.tinhChuVi(),3));
+            Console.ReadLine();
         }
     }
 }
